35b3d5d7ef9d4d0315136a1db78c2b10c2d6fac8
Mostafa-100 <imustaphaaourik@gmail.com>
2025-06-05 15:41:52 +0100

    Adding rate limiter and helmet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const customErrorHandler = require("./middlewares/customErrorHandler");
 
@@ -7,10 +9,26 @@ app.set("view engine", "ejs");
 app.set("views", "/emails");
 
 const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    message:
+      "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+app.use(limiter);
+app.use(helmet());
 
 app.use(express.json());
 
+app.disable("x-powered-by");
+
 app.use("/", authRoutes);
+app.use("/v1/api", productRoutes);
 
 app.use(customErrorHandler);
 
